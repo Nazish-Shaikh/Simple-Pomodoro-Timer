244049678a83cc3217150f0ca73160ff2301b016
Nazish-Shaikh <87064341+Nazish-Shaikh@users.noreply.github.com>
2021-07-08 10:29:34 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,53 @@
+const focusTimeInput = document.querySelector("#focusTime");
+const breakTimeInput = document.querySelector("#breakTime");
+const pauseBtn = document.querySelector(".pause");
+
+focusTimeInput.value = localStorage.getItem("focusTime");
+breakTimeInput.value = localStorage.getItem("breakTime");
+
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  localStorage.setItem("focusTime", focusTimeInput.value);
+  localStorage.setItem("breakTime", breakTimeInput.value);
+});
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function focusCompleteMessage() {
+  alert("!!Focus Time is Over!!\nClick on 'Start Break' to start Break Timer.");
+}
+
+function breakCompleteMessage() {
+  alert("!!Focus Time is Over!!\nClick on 'Start Break' to start Break Timer.");
+}
+
+document.querySelector(".reset").addEventListener("click", () => {
+  startBtn.style.transform = "scale(1)";
+  clearTimeout(initial);
+  setProgress(0);
+  mindiv.textContent = "00";
+  secdiv.textContent = "00";
+});
+
+pauseBtn.addEventListener("click", () => {
+  if (paused === undefined) {
+    return;
+  }
+  if (paused) {
+    paused = false;
+    initial = setTimeout("decremenT()", 60);
+    pauseBtn.textContent = "Pause";
+    pauseBtn.classList.remove("resume");
+  } else {
+    clearTimeout(initial);
+    pauseBtn.textContent = "Resume";
+    pauseBtn.classList.add("resume");
+    paused = true;
+  }
+});
